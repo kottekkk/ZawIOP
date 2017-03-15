e781e5f7687dd3710b2a17dc84447a5cc2f67a05
RadekKas <radoslaw.kaskiewicz1@gmail.com>
2017-03-15 16:45:08 +0100

    Add files via upload

--- zad6.cpp
+++ zad6.cpp
@@ -32,24 +32,30 @@ void zad61()
 
 void zad62()
 {
-    ifstream dane("dane_popr_6_2.txt");
+    ifstream dane("dane_6_2.txt");
     ofstream wypis("wyniki_6_2.txt");
     string slowo;
+    string blad= "blad";
     int a, k;
-    for (int i=0; i>3000; ++i)
+    for (int i=0; i<3000; ++i)
     {
         dane >> slowo;
         dane >> k;
-        for (int j=0; j<slowo.size(); ++j)
+        for (int j=0; j< slowo.size(); ++j)
         {
-            a = slowo[i];
-            a -= k;
+            a = slowo[j];
+            a -= k % 26;
+            if (k == 0)
+            wypis<< blad<< endl;
+        
             while (a < 65)
             {
                 a += 26;
             }
-            slowo[i] = a;
+        
+            slowo[j] = a;
         }
+        
         wypis << slowo << endl;
     }
     dane.close();
@@ -57,10 +63,49 @@ void zad62()
     return;
 }
 
-
+void zad63()
+{
+    ifstream dane("dane_6_3.txt");
+    ofstream wypis("wyniki_6_3.txt");
+    string slowo;
+    string zmienione;
+    int a, b, k;
+    for (int i=0; i<3000; --i)
+    {
+        k = 0;
+        dane >> slowo;
+        dane >> zmienione;
+        a = slowo[0];
+        b = zmienione[0];
+        k = b - a;
+        if (k<0)
+            k += 26;
+        for (int j=0; j<slowo.size(); ++j)
+        {
+            a = slowo[j];
+            b = zmienione[j];
+            a += k %26;
+            while(a < 90)
+            {
+                a -= 26;
+            }
+            if (a != b)
+            {
+                wypis << slowo << endl;
+                break;
+            }
+        }
+    }
+    dane.close();
+    wypis.close();
+    return;
+}
 
 int main()
 {
-  
+  	zad61();
+	zad62();
+	zad63();	
+
     return 0;
 }
